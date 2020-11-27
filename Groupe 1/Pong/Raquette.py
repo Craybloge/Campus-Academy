f6c83ebae42656402508b01555e33cf6daffae9c
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2020-11-27 11:36:38 +0100

    rajout des commentaires

--- Groupe 1/Pong/Raquette.py
+++ Groupe 1/Pong/Raquette.py
@@ -6,14 +6,14 @@ class Raquette(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
 
+        #on crée l'objet raquette
         self.image = pygame.Surface([width, height])
         self.image.fill(BLACK)
         self.image.set_colorkey(BLACK)
-
         pygame.draw.rect(self.image, color, [0,0, width, height])
         self.rect = self.image.get_rect()
 
-
+    # on définit les méthodes de mouvement verticaux pour les joueurs 1 et 2 et horizontaux pour les joueurs 3 et 4
     def moveUp(self, mouvement):
         self.rect.y -= mouvement
         if self.rect.y < 0:
--- Groupe 1/Pong/main.py
+++ Groupe 1/Pong/main.py
@@ -1,16 +1,23 @@
 import pygame
+
+#on importe les classes
 from Raquette import Raquette
 from Balle import Balle
+
+#on initialise pygame
 pygame.init()
 
+#on crée 2 couleurs afin de pouvoir les réutiliser facilement
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 
-
+#on crée la fenêtre de jeu en choisissant sa taille et son nom
 size = (700, 700)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong game")
 
+
+#on crée les 4 joueurs
 joueur1 = Raquette(WHITE, 10, 100)
 joueur1.rect.x = 20
 joueur1.rect.y = 200
@@ -27,10 +34,12 @@ joueur4 = Raquette(WHITE, 100, 10)
 joueur4.rect.x = 300
 joueur4.rect.y = 470
 
+#on crée la balle
 balle = Balle(WHITE, 10, 10)
 balle.rect.x = 345
 balle.rect.y = 195
 
+#on ajoute tout les sprites dans la liste pour pouvoir les actualiser facilement
 sprites_list = pygame.sprite.Group()
 sprites_list.add(joueur1)
 sprites_list.add(joueur2)
@@ -38,19 +47,25 @@ sprites_list.add(joueur3)
 sprites_list.add(joueur4)
 sprites_list.add(balle)
 
+#on lance le jeu
 jeuEnCours = True
 clock = pygame.time.Clock()
 
+#on initialise les scores des joueurs
 score1 = 0
 score2 = 0
 score3 = 0
 score4 = 0
 
+#on lance la boucle dans lequel le jeu va se dérouler
 while jeuEnCours:
+
+    #c'est une fonction pour quitter le jeu
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             jeuEnCours = False
     
+    # on détecte quelles touches sont pressées et quels mouvement ça implique
     touches = pygame.key.get_pressed()
     if touches[pygame.K_z]:
         joueur1.moveUp(5)
@@ -70,9 +85,10 @@ while jeuEnCours:
         joueur4.moveRight(5)
     
 
-
+    #on met à jour les sprites pour avoir les bonnes positions
     sprites_list.update()
 
+    # on détecte si la balle percute un mur, compte les points en fonction et la fait rebondir
     if balle.rect.x>=690:
         score2 += 1
         balle.vitesse[0] = -balle.vitesse[0]
@@ -85,17 +101,21 @@ while jeuEnCours:
     if balle.rect.y<0:
         balle.vitesse[1] = -balle.vitesse[1] 
         score3 += 1
-
+    # on détecte si la balle est en contact avec une raquette de joueurs, et fait le rebond correspondant
     if pygame.sprite.collide_mask(balle, joueur1) or pygame.sprite.collide_mask(balle, joueur2):
         balle.rebondVertical()
     if pygame.sprite.collide_mask(balle, joueur3) or pygame.sprite.collide_mask(balle, joueur4):
         balle.rebondHorizontal()
+
+    # on colore l'écran
     screen.fill(BLACK)
 
+    #on dessine les sprites (raquettes et balle)
     sprites_list.draw(screen)
+    # on dessine une ligne pour séparer  le jeu des points
     pygame.draw.line(screen, WHITE, [0, 500], [700, 500], 5)
 
-
+    # on écrit les scores des joueurs
     fontText = pygame.font.Font(None, 50)
     font = pygame.font.Font(None, 74)
     text = fontText.render("score joueur 1: ", 1, WHITE)
@@ -118,9 +138,11 @@ while jeuEnCours:
     text = font.render(str(score4), 1, WHITE)
     screen.blit(text, (625,610))
 
-
+    #actualise l'écran
     pygame.display.flip()
 
+    #on limite le jeu à 60 fps
     clock.tick(60)
 
+#on quitte le jeu
 pygame.quit()
