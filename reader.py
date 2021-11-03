f3e8c0dc4db557deda4712ac4a2e862e85ad297d
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2021-11-03 15:10:09 +0100

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,17 +1,14 @@
-#le parametre d'entrer c'est "liste"
+# le parametre d'entrer c'est "liste"
+
+def mise_en_forme():
+    nb_of_letter_max = 0
+    for i in range(len(liste)):
+        for j in range(len(liste[i])):
+            temp = len(liste[i][j])
+            if temp > nb_of_letter_max:
+                nb_of_letter_max = temp
 
 
-
-
-
-nb_of_letter_max = 0
-for i in range(len(liste)):
-    for j in range(len(liste[i])):
-        temp = len(liste[i][j])
-        if temp > nb_of_letter_max:
-            nb_of_letter_max = temp
-
-
-for i in range(len(liste)):
-    for j in range(len(liste[i])):
-        print(liste[i][j] , " " * (nb_of_letter_max - len(liste[i][j])), "|")
+    for i in range(len(liste)):
+        for j in range(len(liste[i])):
+            print(liste[i][j], " " * (nb_of_letter_max - len(liste[i][j])), "|")
