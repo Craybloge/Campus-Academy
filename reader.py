828c680b877a9e35d9ae00268a4d9ef844469624
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2021-11-03 15:41:05 +0100

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,14 +1,23 @@
 # le parametre d'entrer c'est "liste"
 
-def mise_en_forme():
-    nb_of_letter_max = 0
-    for i in range(len(liste)):
-        for j in range(len(liste[i])):
-            temp = len(liste[i][j])
-            if temp > nb_of_letter_max:
-                nb_of_letter_max = temp
+liste = (["dsfsdsdfsd", "FKEPJFPZdddddddddddddddddddddFfjfiez", "sdvvzefz"],
+         ["zefzfzfefz", "dzfsdvze"])
 
 
+def mise_en_forme(liste):
+    liste_nb_max = [0]*len(liste[0])
+    temp = [0]*len(liste[0])
     for i in range(len(liste)):
         for j in range(len(liste[i])):
-            print(liste[i][j], " " * (nb_of_letter_max - len(liste[i][j])), "|")
+            temp[j] = len(liste[i][j])
+            if temp[j] > liste_nb_max[j] :
+                liste_nb_max[j] = temp[j]
+
+    for i in range(len(liste)):
+        print(" ")
+        for j in range(len(liste[i])):
+            print(" ", liste[i][j], " " *
+                  (liste_nb_max[j] - len(liste[i][j])), "|", end="")
+
+
+mise_en_forme(liste)
