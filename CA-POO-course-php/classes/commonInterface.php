4aeaa3f09da0d1d1745e0ab1517ae3cf8416c2a7
Mathieu Pithon <craybloge@gmail.com>
2022-01-05 09:28:29 +0100

    suppression du bordel bugué

--- CA-POO-course-php/classes/commonInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-interface CommonInterface {
-
-    public function volume();
-
-}
--- CA-POO-course-php/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once('classes/fileReader.php');
-
-$fr = new FileReader();
-foreach ($fr->annualFiles as $af) {
-    echo("<br>lecture du fichier : " . $af . "<br>");
-    $fr->readFile($af);
-}
